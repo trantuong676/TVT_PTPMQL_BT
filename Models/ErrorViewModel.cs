52be562816d8e8506d6714ea4d6cd196c860a496
trantuong676 <128342689+trantuong676@users.noreply.github.com>
2023-10-24 08:00:53 +0700

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TVT_PTPMQL_BT.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
